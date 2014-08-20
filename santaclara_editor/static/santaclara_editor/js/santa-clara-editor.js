6d0d39b5ebef3ced8c78c1a23ea9dcc1c091e1f8
Chiara Paci <chiara.paci@gmail.com>
2014-08-20 12:33:16 +0200

    New append decorator 45

--- santaclara_editor/static/santaclara_editor/js/santa-clara-editor.js
+++ santaclara_editor/static/santaclara_editor/js/santa-clara-editor.js
@@ -14,7 +14,12 @@ $(function(){
 	    if (mutation.addedNodes.length==0) return;
 
 	    $(mutation.addedNodes).each(function(){
-		console.log($(this));
+		if (typeof $(this) == "object") {
+		    console.log($(this));
+		}
+		else {
+		    console.log(typeof $(this));
+		}
 		$(this).children(".santa-clara-editor").each(function(){
 		    var ta_id=$(this).data("ta_id");
 		    var name=$(this).data("ta_name");
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='santaclara_editor',
-    version='0.3.3.44',
+    version='0.3.3.45',
     packages=['santaclara_editor'],
     requires=['santaclara_third'],
     package_data={'santaclara_editor': [
