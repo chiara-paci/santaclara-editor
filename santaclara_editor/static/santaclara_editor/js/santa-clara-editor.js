35e8364a24e318ef0e16a325e95bb70a0bae3b37
Chiara Paci <chiara.paci@gmail.com>
2014-08-20 14:42:29 +0200

    New append decorator 54

--- santaclara_editor/static/santaclara_editor/js/santa-clara-editor.js
+++ santaclara_editor/static/santaclara_editor/js/santa-clara-editor.js
@@ -20,8 +20,8 @@ $(function(){
 		});
 		*/
 		$(this).find(".santa-clara-editor").each(function(){
-		    var ta_id=$(this).data("ta_id").replace(/^santa_clara_/,"");
-		    var name=$(this).data("ta_name").replace(/^santa_clara_/,"");
+		    var ta_id=$(this).attr("id").replace(/^santa_clara_/,"");
+		    var name=$(this).attr("name").replace(/^santa_clara_/,"");
 		    console.log($(this),ta_id,name);
 		    
 		    /*
