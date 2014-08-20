eacddc407984d0a00ddcc78189aabb888c187b30
Chiara Paci <chiara.paci@gmail.com>
2014-08-20 10:04:49 +0200

    New append decorator 16

--- santaclara_editor/widgets.py
+++ santaclara_editor/widgets.py
@@ -121,7 +121,7 @@ class SantaClaraJQueryUIWidget(forms.Textarea):
 
         #H+='$("#santa_clara_'+ta_id+'").closest("form").hook("append",append_interceptor);\n'
         #H+='$("#santa_clara_'+ta_id+'").closest("form").hook("insertBefore",append_interceptor);\n'
-        H+='console.log($("#santa_clara_'+ta_id+'").closest("fieldset").parent());\n'
+        H+='console.log($("#santa_clara_'+ta_id+'").closest("fieldset").parent().attr("id"));\n'
         #H+='$("#santa_clara_'+ta_id+'").closest("fieldset").parent().hook("clone",append_interceptor);\n'
 
         H+='$("#santa_clara_'+ta_id+'").santa_clara_editor({'
