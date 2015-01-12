d6b857cb495a18ad3cb2fea427880b0a2d33780c
Chiara Paci <chiara.paci@gmail.com>
2015-01-12 15:57:00 +0100

    Add Ace based widget SantaClaraAceWidget 5

--- santaclara_editor/widgets.py
+++ santaclara_editor/widgets.py
@@ -127,141 +127,74 @@ class SantaClaraAceWidget(forms.Textarea):
               'santaclara_editor/js/ace_widget.js'
               )
 
-
-    # <div id="santa_clara_text_editor_box_{{ scene.id }}" class="santa_clara_text_editor_box">
-    #   <!-- editor button bar -->
-    #   <div id="santa_clara_text_editor_button_bar_{{ scene.id }}" class="santa_clara_text_editor_button_bar"
-    #        data-santa_clara_text_editor_box_id="santa_clara_text_editor_box_{{ scene.id }}"
-    #        data-santa_clara_text_editor_id="santa_clara_text_editor_{{ scene.id }}">
-
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="left">
-    #       <i class="editor-button fa fa-align-left"></i><span class="tooltip">align left</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="center">
-    #       <i class="editor-button fa fa-align-center"></i><span class="tooltip">align center</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="right">
-    #       <i class="editor-button fa fa-align-right"></i><span class="tooltip">align right</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="justify">
-    #       <i class="editor-button fa fa-align-justify"></i><span class="tooltip">justify</span></a>
-
-    #     <span class="hspace" style="padding-left:1em;"></span>
-
-    #     <a href="" class="santa_clara_text_editor_button_single" data-tag="hspace">
-    #       <i class="editor-button fa fa-arrows-h"></i><span class="tooltip">add horizontal space</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_single" data-tag="vspace">
-    #       <i class="editor-button fa fa-arrows-v"></i><span class="tooltip">add vertical space</span></a>
-
-    #     <span class="hspace" style="padding-left:1em;"></span>
-
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="b">
-    #       <b><span class="editor-button" style="padding-left:.2em;padding-right:.2em;">B</span></b><span class="tooltip">bold</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="i">
-    #       <i><span class="editor-button" style="padding-left:.2em;padding-right:.2em;">I</span></i><span class="tooltip">italic</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="s">
-    #       <span class="editor-button linethrough" style="padding-left:.2em;padding-right:.2em;">S</span>
-    #       <span class="tooltip">linethrough</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="u">
-    #       <span class="editor-button underline" style="padding-left:.2em;padding-right:.2em;">U</span><span class="tooltip">underline</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="o">
-    #       <span class="editor-button overline" style="padding-left:.2em;padding-right:.2em;">O</span><span class="tooltip">overline</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="t">
-    #       <tt><span class="editor-button" style="padding-left:.2em;padding-right:.2em;">T</span></tt>
-    #       <span class="tooltip">terminal type</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="sc">
-    #       <span class="editor-button smallcaps" style="padding-left:.2em;padding-right:.2em;">SC</span><span class="tooltip">smallcaps</span></a>
-
-    #     <span class="hspace" style="padding-left:1em;"></span>
-
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="code">
-    #       <i class="editor-button fa fa-file"></i><span class="tooltip">code</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="term">
-    #       <i class="editor-button fa fa-desktop"></i><span class="tooltip">terminal</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="quote">
-    #       <i class="editor-button fa fa-quote-right"></i><span class="tooltip">quote</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_simple" data-tag="reading">
-    #       <i class="editor-button fa fa-newspaper-o"></i><span class="tooltip">reading text (mail, paper, ecc.)</span></a>
-
-    #     <span class="hspace" style="padding-left:1em;"></span>
-
-    #     <a href="" class="santa_clara_text_editor_button_function" data-tag="action_upper">
-    #       a⇾A<span class="tooltip">upper case</span></a>
-    #     <a href="" class="santa_clara_text_editor_button_function" data-tag="action_lower">
-    #       A⇾a<span class="tooltip">lower case</span></a>
-
-    #   </div><!-- editor button bar -->
-
-    #   <div id="santa_clara_text_editor_{{ scene.id }}" class="santa_clara_text_editor">{{ scene.text }}</div>
-
-    # </div><!-- editor box -->
-
-
-
     def render(self, name, value, attrs=None):
+        print "RENDER"
         html = super(SantaClaraAceWidget, self).render(name, value, attrs=attrs)
         ta_id=attrs["id"]
 
-        html='<div id="santa_clara_text_editor_box_'+ta_id+'" class="santa_clara_text_editor_box"'
-        html+=' data-ta_id="'+ta_id+'"'
-        html+=' data-name="'+name+'">'
-        html+='<div id="santa_clara_text_editor_button_bar_'+ta_id+'" class="santa_clara_text_editor_button_bar"'
-        html+=' data-santa_clara_text_editor_box_id="santa_clara_text_editor_box_'+ta_id+'"'
-        html+=' data-santa_clara_text_editor_id="santa_clara_text_editor_'+ta_id+'">'
+        html=u'<div id="santa_clara_text_editor_box_'+ta_id+'" class="santa_clara_text_editor_box"'
+        html+=u' data-ta_id="'+ta_id+'"'
+        html+=u' data-name="'+name+'">'
+        html+=u'<div id="santa_clara_text_editor_button_bar_'+ta_id+'" class="santa_clara_text_editor_button_bar"'
+        html+=u' data-santa_clara_text_editor_box_id="santa_clara_text_editor_box_'+ta_id+'"'
+        html+=u' data-santa_clara_text_editor_id="santa_clara_text_editor_'+ta_id+'">'
 
         for tag in [ "left","center","right","justify" ]:
-            html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="'+tag+'">'
-            html+='  <i class="editor-button fa fa-align-'+tag+'"></i><span class="tooltip">align '+tag+'</span></a>'
+            html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="'+tag+'">'
+            html+=u'  <i class="editor-button fa fa-align-'+tag+'"></i><span class="tooltip">align '+tag+'</span></a>'
 
-        html+='<span class="hspace" style="padding-left:1em;"></span>'
+        html+=u'<span class="hspace" style="padding-left:1em;"></span>'
 
-        html+='<a href="" class="santa_clara_text_editor_button_single" data-tag="hspace">'
-        html+='  <i class="editor-button fa fa-arrows-h"></i><span class="tooltip">add horizontal space</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_single" data-tag="vspace">'
-        html+='  <i class="editor-button fa fa-arrows-v"></i><span class="tooltip">add vertical space</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_single" data-tag="hspace">'
+        html+=u'  <i class="editor-button fa fa-arrows-h"></i><span class="tooltip">add horizontal space</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_single" data-tag="vspace">'
+        html+=u'  <i class="editor-button fa fa-arrows-v"></i><span class="tooltip">add vertical space</span></a>'
 
-        html+='<span class="hspace" style="padding-left:1em;"></span>'
+        html+=u'<span class="hspace" style="padding-left:1em;"></span>'
 
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="b">'
-        html+='  <b><span class="editor-button" style="padding-left:.2em;padding-right:.2em;">B</span></b><span class="tooltip">bold</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="i">'
-        html+='  <i><span class="editor-button" style="padding-left:.2em;padding-right:.2em;">I</span></i><span class="tooltip">italic</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="s">'
-        html+='  <span class="editor-button linethrough" style="padding-left:.2em;padding-right:.2em;">S</span>'
-        html+='  <span class="tooltip">linethrough</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="u">'
-        html+='  <span class="editor-button underline" style="padding-left:.2em;padding-right:.2em;">U</span><span class="tooltip">underline</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="o">'
-        html+='  <span class="editor-button overline" style="padding-left:.2em;padding-right:.2em;">O</span><span class="tooltip">overline</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="t">'
-        html+='  <tt><span class="editor-button" style="padding-left:.2em;padding-right:.2em;">T</span></tt>'
-        html+='  <span class="tooltip">terminal type</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="sc">'
-        html+='  <span class="editor-button smallcaps" style="padding-left:.2em;padding-right:.2em;">SC</span><span class="tooltip">smallcaps</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="b">'
+        html+=u'  <b><span class="editor-button" style="padding-left:.2em;padding-right:.2em;">B</span></b><span class="tooltip">bold</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="i">'
+        html+=u'  <i><span class="editor-button" style="padding-left:.2em;padding-right:.2em;">I</span></i><span class="tooltip">italic</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="s">'
+        html+=u'  <span class="editor-button linethrough" style="padding-left:.2em;padding-right:.2em;">S</span>'
+        html+=u'  <span class="tooltip">linethrough</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="u">'
+        html+=u'  <span class="editor-button underline" style="padding-left:.2em;padding-right:.2em;">U</span><span class="tooltip">underline</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="o">'
+        html+=u'  <span class="editor-button overline" style="padding-left:.2em;padding-right:.2em;">O</span><span class="tooltip">overline</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="t">'
+        html+=u'  <tt><span class="editor-button" style="padding-left:.2em;padding-right:.2em;">T</span></tt>'
+        html+=u'  <span class="tooltip">terminal type</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="sc">'
+        html+=u'  <span class="editor-button smallcaps" style="padding-left:.2em;padding-right:.2em;">SC</span><span class="tooltip">smallcaps</span></a>'
         
-        html+='<span class="hspace" style="padding-left:1em;"></span>'
+        html+=u'<span class="hspace" style="padding-left:1em;"></span>'
         
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="code">'
-        html+='  <i class="editor-button fa fa-file"></i><span class="tooltip">code</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="term">'
-        html+='  <i class="editor-button fa fa-desktop"></i><span class="tooltip">terminal</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="quote">'
-        html+='  <i class="editor-button fa fa-quote-right"></i><span class="tooltip">quote</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_simple" data-tag="reading">'
-        html+='  <i class="editor-button fa fa-newspaper-o"></i><span class="tooltip">reading text (mail, paper, ecc.)</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="code">'
+        html+=u'  <i class="editor-button fa fa-file"></i><span class="tooltip">code</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="term">'
+        html+=u'  <i class="editor-button fa fa-desktop"></i><span class="tooltip">terminal</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="quote">'
+        html+=u'  <i class="editor-button fa fa-quote-right"></i><span class="tooltip">quote</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_simple" data-tag="reading">'
+        html+=u'  <i class="editor-button fa fa-newspaper-o"></i><span class="tooltip">reading text (mail, paper, ecc.)</span></a>'
         
-        html+='<span class="hspace" style="padding-left:1em;"></span>'
+        html+=u'<span class="hspace" style="padding-left:1em;"></span>'
         
-        html+='<a href="" class="santa_clara_text_editor_button_function" data-tag="action_upper">'
-        html+='  a⇾A<span class="tooltip">upper case</span></a>'
-        html+='<a href="" class="santa_clara_text_editor_button_function" data-tag="action_lower">'
-        html+='  A⇾a<span class="tooltip">lower case</span></a>'
-        html+='</div><!-- text_editor_button_bar -->'
+        html+=u'<a href="" class="santa_clara_text_editor_button_function" data-tag="action_upper">'
+        html+=u'  a⇾A<span class="tooltip">upper case</span></a>'
+        html+=u'<a href="" class="santa_clara_text_editor_button_function" data-tag="action_lower">'
+        html+=u'  A⇾a<span class="tooltip">lower case</span></a>'
+        html+=u'</div><!-- text_editor_button_bar -->'
 
-        html+='<div id="santa_clara_text_editor_'+ta_id+'" class="santa_clara_text_editor"'
-        html+=' data-ta_id="'+ta_id+'"'
-        html+=' data-name="'+name+'">'
+        html+=u'<div id="santa_clara_text_editor_'+ta_id+'" class="santa_clara_text_editor"'
+        html+=u' data-ta_id="'+ta_id+'"'
+        html+=u' data-name="'+name+'">'
         html+=unicode(value)
-        html+='</div>'
+        html+=u'</div>'
 
-        html+='</div><!-- text_editor_box -->'
+        html+=u'</div><!-- text_editor_box -->'
 
         print html
         
