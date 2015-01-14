be57a975253f1b7d1243fc84d1a78b6c73147846
Chiara Paci <chiara.paci@gmail.com>
2015-01-14 16:41:20 +0100

    Fix square bracket 5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='santaclara_editor',
-    version='0.3.7.4',
+    version='0.3.7.5',
     packages=['santaclara_editor'],
     requires=['santaclara_third'],
     package_data={'santaclara_editor': [
